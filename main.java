f14d22bc0c0411dd6bef5cadbcf590075c3d97a4
cmccloney <cmccloney@hotmail.com>
2018-11-06 14:34:09 -0700

    frustrated

--- main.java
+++ main.java
@@ -1,1 +1,13 @@
 //test
+
+public class mccloney-3{
+	public static void main(String[] args) {
+		Job T1 = new Job();
+		Job T2 = new Job();
+		Job T3 = new Job();
+	}
+}
+
+public class Job() {
+	
+}
